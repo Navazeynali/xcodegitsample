eee533bafdf206c15cd4267c33b3d7e53a26fb44
Nava zeynali <navazeynali00@gmail.com>
2023-10-17 15:04:11 +0330

    Initial Commit

---
+++ gith/githApp.swift
@@ -0,0 +1,17 @@
+//
+//  githApp.swift
+//  gith
+//
+//  Created by nava on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct githApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
